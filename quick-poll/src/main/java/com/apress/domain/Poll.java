036fd8d06fc75cb7280884288d4b48ddc13adfe7
adellapo <andres.m.dellaporta@gmail.com>
2019-12-30 10:05:45 -0300

    Primer commit

---
+++ quick-poll/src/main/java/com/apress/domain/Poll.java
@@ -0,0 +1,62 @@
+package com.apress.domain;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+
+@Entity
+public class Poll {
+
+	@Id
+	@GeneratedValue
+	@Column(name="POLL_ID")
+	private Long id;
+	
+	@Column(name="QUESTION")
+	private String question;
+	
+	@Column(name="POLL_NAME")
+	private String name;
+	
+	@OneToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="POLL_ID")
+	@OrderBy
+	private Set<Option> options;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public Set<Option> getOptions() {
+		return options;
+	}
+	public void setOptions(Set<Option> options) {
+		this.options = options;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return getId() + ", " + getQuestion() + ", " + getOptions();
+	}
+	
+}
