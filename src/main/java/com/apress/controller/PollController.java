c72783b514f74c003af8d41bfa45138f23a0062e
adellapo <andres.m.dellaporta@gmail.com>
2021-06-17 00:33:03 -0300

    Se sube a Java 1.8 y se refactorea PollController

--- src/main/java/com/apress/controller/PollController.java
+++ src/main/java/com/apress/controller/PollController.java
@@ -1,8 +1,8 @@
 package com.apress.controller;
 
 import java.net.URI;
-import javax.inject.Inject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,46 +19,125 @@ import com.apress.repository.PollRepository;
 @RestController
 public class PollController {
 
-	@Inject
+	@Autowired
 	private PollRepository pollRepository;
-	
-	@RequestMapping(value="/polls", method=RequestMethod.POST)
+
+	// creo una Encuesta. Con @RequestBody se va estar vinculando el body de la peticion
+	@RequestMapping(value = "/polls", method = RequestMethod.POST)
 	public ResponseEntity<?> createPoll(@RequestBody Poll poll) {
-		
-		// recibo el elemento Poll mediante la variable poll y lo guardo 
+
+		// recibo el elemento Poll mediante la variable poll y lo guardo
 		poll = pollRepository.save(poll);
-		
-		// Set the location header for the newly created resource
+	
+		// creo los headers que van a ser enviados en la respuesta
 		HttpHeaders responseHeaders = new HttpHeaders();
-		URI newPollUri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(poll.getId()).toUri();
-		responseHeaders.setLocation(newPollUri);
+		URI newPollUri = ServletUriComponentsBuilder
+							.fromCurrentRequest() 			// ver NOTAS 1 - ServletUriComponentsBuilder
+							.path("/{id}")					// ver NOTAS 2 - UriComponentsBuilder
+							.buildAndExpand(poll.getId())	// ver NOTAS 3 - UriComponentsBuilder
+							.toUri();						// genera la URI
+		responseHeaders.setLocation(newPollUri);			// seteo la ubicacion del header para la nueva entidad creada
 		
+		// envio respuesta 201 (creado)
+		// body: null
+		// headers: responseHeaders
+		// statusCode: 201
 		return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
 	}
 
-	@RequestMapping(value="/polls", method=RequestMethod.GET)
+	// obtengo las Encuestas
+	@RequestMapping(value = "/polls", method = RequestMethod.GET)
 	public ResponseEntity<Iterable<Poll>> getAllPolls() {
+
+		// obtengo un Iterable que es una interfaz que permite
+		// a un objeto ser el objetivo de un blucle for-each
 		Iterable<Poll> allPolls = pollRepository.findAll();
+
+		// envio la respuesta 200 (ok, salio todo bien) pasando el iterable que
+		// representa todas las entidades encuestas
+		// encontradas por el repositorio
 		return new ResponseEntity<>(allPolls, HttpStatus.OK);
+
 	}
 
-	@RequestMapping(value="/polls/{pollId}", method=RequestMethod.GET)
+	// obtengo una Encuesta a partir de su id
+	@RequestMapping(value = "/polls/{pollId}", method = RequestMethod.GET)
 	public ResponseEntity<?> getPoll(@PathVariable Long pollId) {
+
+		// instancio una entidad Encuesta consultando al repositorio
 		Poll p = pollRepository.findOne(pollId);
-		return new ResponseEntity<> (p, HttpStatus.OK);
+
+		// envio una respuesta 200 (OK) pasando pasandole la entidad Encuesta encontrada
+		return new ResponseEntity<>(p, HttpStatus.OK);
 	}
-	
-	@RequestMapping(value="/polls/{pollId}", method=RequestMethod.PUT)
+
+	// actualizo la Encuesta a partir de un id
+	@RequestMapping(value = "/polls/{pollId}", method = RequestMethod.PUT)
 	public ResponseEntity<?> updatePoll(@RequestBody Poll poll, @PathVariable Long pollId) {
-		// Save the entity
-		Poll p = pollRepository.save(poll);
+
+		// guardo la Encuesta en el repositorio
+		pollRepository.save(poll);
+
+		// envio respuesta 200 (OK)
 		return new ResponseEntity<>(HttpStatus.OK);
+
 	}
-	
-	@RequestMapping(value="/polls/{pollId}", method=RequestMethod.DELETE)
+
+	// borro una Encuesta a partir de su id
+	@RequestMapping(value = "/polls/{pollId}", method = RequestMethod.DELETE)
 	public ResponseEntity<?> deletePoll(@PathVariable Long pollId) {
+
+		// borro la Encuesta del repositorio
 		pollRepository.delete(pollId);
+
+		// envio respuesta 200 (OK)
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	
+
 }
+
+//------------------------------------------------------------------------------------
+// NOTAS
+//------------------------------------------------------------------------------------
+/*
+ * 1:
+ * --
+ * Same as fromRequest(HttpServletRequest) except the request is obtained 
+ * through RequestContextHolder:
+ * 
+ * public static ServletUriComponentsBuilder fromCurrentRequest() {
+ * 		return fromRequest(getCurrentRequest());
+ * }
+ * ------------------------------------------------------------------------------------
+ * Prepare a builder from the host, port, scheme, and path (but not the query)
+ * of the HttpServletRequest:
+ * 
+ * public static ServletUriComponentsBuilder fromRequest(HttpServletRequest request){
+ * 	...
+ * }
+ * ------------------------------------------------------------------------------------
+ * protected static HttpServletRequest getCurrentRequest() {
+ * 		RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+ * 		...
+ * 		HttpServletRequest servletRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
+ * 		return servletRequest;
+ * 	}
+ * ------------------------------------------------------------------------------------
+ * 2:
+ * --
+ * 	Adjunta la path dada a la path existente del UriComponentsBuilder. 
+ * 	La path dada puede contener variables de template de UR
+ * ------------------------------------------------------------------------------------
+ * 3:
+ * --
+ * 	crea una instancia de UriComponents y reemplaza las variables plantilla de URI 
+ * 	por los valores del arreglo
+ * ------------------------------------------------------------------------------------
+ * 4:
+ * --
+ * 	@RequestBody indica que un parametro de metodo debe estar vinculada al cuerpo de la solicitu web
+ *  El cuerpo de la solicitud se pasa a través de un HttpMessageConverter para resolver el
+  * argumento del método según el tipo de contenido de la solicitud. Opcionalmente, la validación 
+  * automática se puede aplicar anotando el argumento con @Valid
+  *
+ */	
