cec4ef4f32a42e608b582ca6e0649a6da9136081
adellapo <andres.m.dellaporta@gmail.com>
2021-06-30 01:17:50 -0300

    Se agrega messages.properties y se configura RestExceptionHandler para extraer los mensajes de error desde el archivo

--- src/main/java/com/apress/handler/RestExceptionHandler.java
+++ src/main/java/com/apress/handler/RestExceptionHandler.java
@@ -6,12 +6,16 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.apress.controller.error.ErrorDetail;
 import com.apress.controller.error.ValidationError;
@@ -19,6 +23,9 @@ import com.apress.exception.ResourceNotFoundException;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+
+	@Autowired
+	MessageSource messageSource;
 
 	// cada vez que se invoque un ResourceNotFoundException se viene aca a ejecutar handlerResourceNotFoundException
 	@ExceptionHandler(value = ResourceNotFoundException.class)
@@ -37,7 +44,9 @@ public class RestExceptionHandler {
 	}
 	
 	@ExceptionHandler(value = MethodArgumentNotValidException.class)
-	public ResponseEntity<?> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request) {
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public @ResponseBody ErrorDetail handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request) {
+		
 		
 		ErrorDetail errorDetail = new ErrorDetail();
 
@@ -70,10 +79,50 @@ public class RestExceptionHandler {
 			}
 			ValidationError validationError = new ValidationError();
 			validationError.setCode(fe.getCode());
-			validationError.setMessage(fe.getField() + " " + fe.getDefaultMessage());
+			validationError.setMessage(messageSource.getMessage(fe, null));
 			validationErrorList.add(validationError);
 		}
-		return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+		return errorDetail;
 	}
 	
+//	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+//	public ResponseEntity<?> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request) {
+//		
+//		ErrorDetail errorDetail = new ErrorDetail();
+//		
+//		errorDetail.setTimeStamp(new Date().getTime());
+//		errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+//		
+//		String requestPath = (String) request.getAttribute("javax.servlet.error.request_uri");
+//		
+//		if (requestPath == null) {
+//			requestPath = request.getRequestURI();
+//		}
+//		
+//		errorDetail.setTitle("Validation failed");
+//		errorDetail.setDetail("Input validation failed");
+//		errorDetail.setDeveloperMessage(exception.getClass().getName());
+//		
+//		// creo las instancias de FieldError
+//		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+//		
+//		// itero los field errors
+//		for (FieldError fe : fieldErrors) {
+//			
+//			// obtengo una lista de ValidationError pasandole el campo como key que va a buscar al Map errors ==> ValidationError.errors
+//			List<ValidationError> validationErrorList = errorDetail.getErrors().get(fe.getField());
+//			
+//			// si no hay un ValidationError en el map de errorDetail.errors devuelve null
+//			if (validationErrorList == null) {
+//				validationErrorList = new ArrayList<ValidationError>();
+//				errorDetail.getErrors().put(fe.getField(), validationErrorList);
+//			}
+//			ValidationError validationError = new ValidationError();
+//			validationError.setCode(fe.getCode());
+//			validationError.setMessage(fe.getField() + " " + fe.getDefaultMessage());
+//			validationErrorList.add(validationError);
+//		}
+//		return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+//	}
+
 }
